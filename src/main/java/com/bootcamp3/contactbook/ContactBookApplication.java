2589f84e26a37cdca4ba3ef351df05c57e2fd2b2
Sabrina Karen <sabrinaa.karen@hotmail.com>
2021-09-16 14:58:13 -0300

    subida inicial - estrutura spring project

---
+++ src/main/java/com/bootcamp3/contactbook/ContactBookApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp3.contactbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp3/contactbook/ContactBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp3.contactbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
